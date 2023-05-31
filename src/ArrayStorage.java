ec71e08309fbf6688d1df36496f60fbd78588743
Александр Бушнов <abushnov@cleverence.onmicrosoft.com>
2023-05-31 17:42:41 +0300

    Добавлено поле size, остальные методы переделаны на работу с новым полем

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,67 +5,49 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        int resumeQty = this.size();
-        for (int i = 0; i < resumeQty; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i] != null) {
                 storage[i] = null;
             }
-            else {
-                break;
-            }
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        int freeSpaceIndex = this.size();
-        storage[freeSpaceIndex] = r;
+        storage[size] = r;
+        this.size++;
     }
 
     Resume get(String uuid) {
-        Resume result = null;
-        int resumeQty = this.size();
-        for (int i = 0; i < resumeQty; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                result = storage[i];
-                break;
+                return storage[i];
             }
         }
-        return result;
+        return null;
     }
 
     void delete(String uuid) {
-        int resumeQty = this.size();
-        for (int i = 0; i < resumeQty; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                for (int j = i + 1; j < resumeQty; j++) {
-                    storage[j - 1] = storage[j];
-                    storage[j] = null;
-                }
-                break;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int resumesQty = this.size();
-        return Arrays.copyOf(storage, resumesQty);
+        return Arrays.copyOf(storage, this.size());
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume:
-                storage) {
-            if (resume != null) {
-                size++;
-            }
-        }
-        return size;
+        return this.size;
     }
 }
