4f0ec98f7e6d3ffaf7ab7f59d84ed51e11cdbc8a
Александр Бушнов <abushnov@cleverence.onmicrosoft.com>
2023-05-31 16:45:14 +0300

    Реализованы методы save, get, delete, clear, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,65 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int resumeQty = this.size();
+        for (int i = 0; i < resumeQty; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+            else {
+                break;
+            }
+        }
     }
 
     void save(Resume r) {
+        int freeSpaceIndex = this.size();
+        storage[freeSpaceIndex] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = null;
+        int resumeQty = this.size();
+        for (int i = 0; i < resumeQty; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                result = storage[i];
+                break;
+            }
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        int resumeQty = this.size();
+        for (int i = 0; i < resumeQty; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                for (int j = i + 1; j < resumeQty; j++) {
+                    storage[j - 1] = storage[j];
+                    storage[j] = null;
+                }
+                break;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int resumesQty = this.size();
+        return Arrays.copyOf(storage, resumesQty);
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume:
+                storage) {
+            if (resume != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
