24bf754c806d5c200e6ec94fa5a24ba991267ff6
Александр Бушнов <abushnov@cleverence.onmicrosoft.com>
2023-06-13 20:30:19 +0300

    HW3: Implemented AbstractArrayStorage, ArrayStorage, SortedArrayStorage

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -1,26 +1,30 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.storage.SortedArrayStorage;
 
 /**
  * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static final SortedArrayStorage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        final Resume r1 = new Resume();
         r1.setUuid("uuid1");
-        Resume r2 = new Resume();
+        final Resume r2 = new Resume();
         r2.setUuid("uuid2");
-        Resume r3 = new Resume();
+        final Resume r3 = new Resume();
         r3.setUuid("uuid3");
-        Resume r4 = new Resume();
-        r4.setUuid("uuid3");
+        final Resume r4 = new Resume();
+        r4.setUuid("uuid4");
+        final Resume r5 = new Resume();
+        r5.setUuid("uuid5");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r5);
+        ARRAY_STORAGE.save(r4);
         ARRAY_STORAGE.save(r3);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
@@ -34,6 +38,8 @@ public class MainTestArrayStorage {
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
+        printAll();
+        ARRAY_STORAGE.delete(r3.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,81 +2,23 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private static final int STORAGE_LIMIT = 10000;
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size;
+public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-//        if (size == 0) {
-////            return;
-////        }
-        Arrays.fill(storage, 0, size - 1, null);
-        size = 0;
+    protected void saveResume(Resume r, int index) {
+        storage[size] = r;
+        size++;
     }
 
-    public void save(Resume r) {
-        if (size >= storage.length) {
-            System.out.println("ERROR: Storage is full. " +
-                    "Resume with uuid " + r.getUuid() + " wasn't saved.");
-        } else if (getIndex(r.getUuid()) >= 0) {
-            System.out.println("ERROR: Saving wasn't complete. " +
-                    "Resume with uuid " + r.getUuid() + " already presented in the storage");
-        } else {
-            storage[size] = r;
-            size++;
-        }
+    protected void deleteResume(int index) {
+        storage[index] = storage[size - 1];
+        storage[size - 1] = null;
+        size--;
     }
 
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-        } else {
-            System.out.println("ERROR: Update wasn't complete. " +
-                    "Resume with uuid " + r.getUuid() + " is not presented in the storage");
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            return storage[index];
-        }
-        System.out.println("ERROR: No results were found. " +
-                "Resume with uuid \"" + uuid + "\" is not presented in the storage");
-        return null;
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        } else {
-            System.out.println("ERROR: Resume with uuid " + uuid + " is not presented in the storage");
-        }
-    }
-
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
